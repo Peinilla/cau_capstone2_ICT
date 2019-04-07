437c3ed98bcaf2f4628800cdddfd566f867e7f66
Peinilla <33536706+Peinilla@users.noreply.github.com>
2019-04-07 20:38:52 +0900

    0407 작업 (게시판 레이아웃 + 검색창 추가)

--- app/src/main/java/com/melon/cau_capstone2_ict/Manager/MyBoardAdapter.java
+++ app/src/main/java/com/melon/cau_capstone2_ict/Manager/MyBoardAdapter.java
@@ -20,46 +20,33 @@ public class MyBoardAdapter extends BaseAdapter {
     private RecyclerView.ViewHolder viewHolder = null;
 
     public MyBoardAdapter() {
-        Log.d("Tag", "MBA");
-
     }
 
     @Override
     public int getCount() {
-
-        Log.d("Tag", "getCount");
         return listViewItemList.size() ;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Log.d("Tag", "getView_1");
 
         final int pos = position;
         final Context context = parent.getContext();
-        Log.d("Tag", "getView_2");
 
         // "listview_item" Layout을 inflate하여 convertView 참조 획득.
         if (convertView == null) {
-            Log.d("Tag", "null check");
             convertView = LayoutInflater.from(context).
                     inflate(R.layout.listview_myboard, parent, false);
         }
-        Log.d("Tag", "getView_3");
 
-        // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
         TextView titleView = (TextView) convertView.findViewById(R.id.title) ;
         TextView writerView = (TextView) convertView.findViewById(R.id.writer) ;
         TextView commentView = (TextView) convertView.findViewById(R.id.comment) ;
         TextView recommendView = (TextView) convertView.findViewById(R.id.recommend) ;
         TextView dateView = (TextView) convertView.findViewById(R.id.date) ;
-        Log.d("Tag", "getView_4");
 
-        // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
         MyBoard myBoard_item = listViewItemList.get(position);
-        Log.d("Tag", "getView_5");
 
-        // 아이템 내 각 위젯에 데이터 반영
         titleView.setText(myBoard_item.getTitle());
         writerView.setText(myBoard_item.getWriter());
         commentView.setText("("+myBoard_item.getNumComment()+")");
--- app/src/main/java/com/melon/cau_capstone2_ict/TabFragment1.java
+++ app/src/main/java/com/melon/cau_capstone2_ict/TabFragment1.java
@@ -1,6 +1,8 @@
 package com.melon.cau_capstone2_ict;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -8,20 +10,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.R.id.*;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.melon.cau_capstone2_ict.Manager.MyBoardAdapter;
 
 public class TabFragment1 extends Fragment {
 
     MyBoardAdapter adapter;
     ListView listView;
+    BottomNavigationView bnv;
+    FloatingActionButton searchButton;
+    ImageButton searchImage;
+    ImageButton closeImage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.tab_fragment_1, container, false);
 
+        bnv = (BottomNavigationView)rootView.findViewById(R.id.SearchBar) ;
         listView = (ListView) rootView.findViewById(R.id.listview1);
+        searchButton = (FloatingActionButton) rootView.findViewById(R.id.button_search);
+        searchImage = (ImageButton) rootView.findViewById(R.id.image_search);
+        closeImage = (ImageButton) rootView.findViewById(R.id.image_close);
 
         adapter = new MyBoardAdapter();
 
@@ -30,10 +47,61 @@ public class TabFragment1 extends Fragment {
         adapter.addItem();
         adapter.addItem();
         adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+        adapter.addItem();
+
 
 
         listView.setAdapter(adapter);
 
+        searchButton.setOnClickListener(new FloatingActionButton.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                bnv.setVisibility(View.VISIBLE);    }
+        });
+
+        closeImage.setOnClickListener(new ImageButton.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                bnv.setVisibility(View.GONE);          }
+        });
+
+        final Spinner spinner_field = (Spinner) rootView.findViewById(R.id.spinner_search);
+
+
+        String[] str = getResources().getStringArray(R.array.search_item);
+
+
+        final ArrayAdapter<String> adapter= new ArrayAdapter<String>(getContext(),R.layout.spinner_item,str);
+
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        spinner_field.setAdapter(adapter);
+
+
+        spinner_field.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(spinner_field.getSelectedItemPosition() > 0){
+                    Log.v("알림",spinner_field.getSelectedItem().toString()+ "is selected");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
         return rootView;
     }
+
 }
